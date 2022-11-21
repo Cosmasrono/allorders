02dca8522a06c6c16282e7884dcd4f83824f317a
unknown <ccosmas001@gmail.com>
2022-11-21 21:42:01 +0300

    changes

---
+++ src/views/HomeView.vue
@@ -0,0 +1,67 @@
+<script setup>
+ 
+</script>
+
+<template>
+  <main>
+    hello this is an accomadation platform wherre you can find your desired house online without fear or favour you will have what you have asked
+    
+    <div class="  grid  grid-rows-3 md:grid-rows-2 md:grid-cols-2 lg:grid-cols-3 grid-cols-1 items-center justify-center gap-1 mx-auto container">
+              <div class="flex justify-center mx-auto items-center">
+                  <img src="../assets/one.webp"   class=" h-80 w-80 rounded-md" alt="">
+              </div>
+              <div class="flex justify-center mx-auto items-center ">
+                  <img src="../assets/two.jpg"   class=" h-80 w-80 rounded-md" alt="">
+              </div>
+              <div class=" flex justify-center mx-auto items-center">
+                  <img src="../assets/three.jpg"   class=" h-80 w-80 rounded-md"  >
+              </div>
+              <div class=" flex justify-center mx-auto items-center">
+                  <img src="../assets/four.webp"  class=" h-80 w-80 rounded-md"  alt="">
+              </div>
+              <div class=" flex justify-center mx-auto items-center">
+                  <img src="../assets/five.webp"  class=" h-80 w-80 rounded-md"  alt="">
+              </div>
+              <div class="flex justify-center mx-auto items-center ">
+                  <img src="../assets/six.jpg"   class=" h-80 w-80 rounded-md" alt="">
+              </div>
+              </div>
+
+
+              <div class=" pt-3  bg-slate-900 mt-3  hover:scale-90 md:ml-72">
+      <div class=" flex  flex-col md:flex-row justify-center items-center m-5 ">  
+      <div class=" transition-all duration-500 transform hover:opacity-50 pt-1 hover:pb-1">
+        <img src="../assets/lepen.jpg" alt="" class="  w-56 md:mr-80">
+      </div>
+        <p class=" bg-white">This is picture of our  brand new machine TATA<br>
+          Tata Motors strives to bring new products that fire the imagination of GenNext customers, fueled by state of the art design and R&D centers located in India, UK, US, Italy and South Korea. With a focus on engineering and tech enabled automotive solutions catering to the future of mobility, the company’s innovation efforts are focused to develop pioneering technologies that are sustainable as well as suited to evolving aspirations of the market and the customers.
+          </p> 
+      </div>
+      </div>
+
+
+
+
+
+
+<footer class="fixed bottom-0 left-0 z-20 p-4 w-full bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li>
+    </ul>
+</footer>
+
+  </main>
+</template>
